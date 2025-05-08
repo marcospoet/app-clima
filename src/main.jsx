c8ec4f401e534d0ee34f5329de0a6216ee9d1217
Marcos Joaquin Poet <mpoet@frsf.utn.edu.ar>
2025-05-08 11:16:02 -0300

    Refactor styles: renamed 'wheaterStyles.css' to 'wheaterApp.css' and removed unused 'movieSearch.css' and 'wheaterStyles.css' files.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { WheaterApp } from './wheaterApp'
-import './styles/wheaterStyles.css'
+import './styles/wheaterApp.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
